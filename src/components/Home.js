965ab63587c3594bf99fd055ade26a94a147c95f
tnemec <tnemec@arbonne.com>
2018-07-26 15:44:34 -0700

    Home page previous rounds

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Button } from 'react-bootstrap';
+import { Button, Grid, Col, Row, Glyphicon } from 'react-bootstrap';
+import moment from 'moment'
 
 
 
@@ -33,6 +34,7 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      prevRounds: [],
       round: {
       	course: {
       		name : 'Test Name'
@@ -42,7 +44,11 @@ class Home extends Component {
 
   }
   componentDidMount() {
-
+    const history = this.getHistory();
+    if(history && history.length) {
+      // use last 5, sorted by latest first
+      this.setState({prevRounds: history.reverse().slice(0,5)});
+    }
 
   };
 
@@ -54,13 +60,60 @@ class Home extends Component {
     this.props.history.push('/play/' + (this.props.round.currentHole -1));
   };
 
+  formatDate = (str) => {
+    return moment(str).format('MMM Do, YYYY')
+  };
+
+  getHistory = () => {
+    if(window.localStorage && window.localStorage.dgScoreHistory) {
+      try {
+        return JSON.parse(window.localStorage.getItem('dgScoreHistory'))
+      } 
+      catch (e) {
+          console.log('Could not get history data from local storage')
+      }
+    }
+    return undefined
+  };
+
 
   render() {
+
+    const PrevRounds = () => {
+      if(this.state.prevRounds.length) {
+
+        const list = this.state.prevRounds.map( (item) => {
+          if(item) {
+            return (
+              <li key={item.startTime}>
+                <Grid><Row>
+                  <Col md={6}>{item.course.name}</Col>
+                  <Col md={6}>{this.formatDate(item.startTime)} <Glyphicon glyph="eye-open" /></Col>
+                  </Row></Grid>
+                </li>
+            )
+          }
+        });
+
+        return (
+          <div className="section">
+            <h4>Previous Rounds:</h4>
+            <ul className="prev-round-list">
+              {list}
+            </ul>
+          </div>
+        )
+      }
+      return null
+    }
+
     return (
   		<div className="home">
     		<h1>DG Score</h1>
 
-    		<Previous round={this.props.round} courseName={this.props.round.course.name} resumeHandler={this.handleResume}  />    
+    		<Previous round={this.props.round} courseName={this.props.round.course.name} resumeHandler={this.handleResume}  />  
+
+        <PrevRounds />  
 
     		<NewRound newRoundHandler={this.handleNewRound}/>		
     	</div>
